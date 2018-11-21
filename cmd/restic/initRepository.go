bb202bf621b1d3f37e47a8487c98a5b9b9178f6f
Simon Rüegg <simon@rueggs.ch>
2018-11-21 10:16:45 +0100

    Fail if repository can't be accessed

--- cmd/restic/initRepository.go
+++ cmd/restic/initRepository.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func initRepository() error {
-	if _, err := listSnapshots(); err == nil {
+	_, err := listSnapshots()
+	if err == nil {
 		return nil
+	} else if err.Error() == notInitialisedError {
+		fmt.Println("No repository available, initialising...")
+		args := []string{"init"}
+		genericCommand(args, commandOptions{print: true})
+		return commandError
+	} else {
+		return err
 	}
-
-	fmt.Println("No repository available, initialising...")
-	args := []string{"init"}
-	genericCommand(args, commandOptions{print: true})
-	return commandError
 }
