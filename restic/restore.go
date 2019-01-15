936f37d52b9460be62046139bfd168e237e9fb2f
Simon Beck <simon.beck@vshn.ch>
2019-01-15 13:11:44 +0100

    Use dump to tar from restic
    With this change the s3 restore and archival process utilises the newly
    implemented "folder to tar" dump directly from restic. This gives a
    huge speed and stability improvement over the previous implementation
    of this feature.
    
    Relates to: AMZE-1290

--- restic/restore.go
+++ restic/restore.go
@@ -1,8 +1,6 @@
 package restic
 
 import (
-	"archive/tar"
-	"bufio"
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
@@ -30,36 +28,6 @@ type RestoreStruct struct {
 
 func newRestore() *RestoreStruct {
 	return &RestoreStruct{}
-}
-
-type fileJSON []struct {
-	Time       time.Time  `json:"time"`
-	Tree       string     `json:"tree"`
-	Paths      []string   `json:"paths"`
-	Hostname   string     `json:"hostname"`
-	Username   string     `json:"username"`
-	UID        int        `json:"uid"`
-	Gid        int        `json:"gid"`
-	ID         string     `json:"id"`
-	ShortID    string     `json:"short_id"`
-	Nodes      []fileNode `json:"nodes"`
-	StructType string     `json:"struct_type"`
-}
-
-type fileNode struct {
-	Name       string    `json:"name"`
-	Type       string    `json:"type"`
-	Path       string    `json:"path"`
-	UID        int       `json:"uid"`
-	Gid        int       `json:"gid"`
-	Mode       int64     `json:"mode"`
-	Mtime      time.Time `json:"mtime"`
-	Atime      time.Time `json:"atime"`
-	Ctime      time.Time `json:"ctime"`
-	StructType string    `json:"struct_type"`
-	Size       int64     `json:"size,omitempty"`
-	closer     chan io.ReadCloser
-	runFunc    func()
 }
 
 func (r *RestoreStruct) setState(restoreType, restoreDir, restoreFilter string, verifyRestore bool) {
@@ -91,6 +59,12 @@ type restoreStats struct {
 	RestoreLocation string   `json:"restore_location,omitempty"`
 	SnapshotID      string   `json:"snapshot_ID,omitempty"`
 	RestoredFiles   []string `json:"restored_files,omitempty"`
+}
+
+type tarStream struct {
+	path       string
+	readerChan chan io.ReadCloser
+	runFunc    func()
 }
 
 // ToJson implements output.JsonMarshaller
@@ -172,17 +146,6 @@ func (r *RestoreStruct) folderRestore(snapshot Snapshot) error {
 
 func (r *RestoreStruct) s3Restore(snapshot Snapshot) error {
 	fmt.Println("S3 chosen as restore destination")
-	r.listFilesInSnapshot(snapshot)
-	fileList := fileJSON{}
-	out := []byte(strings.Join(r.stdOut, "\n"))
-	err := json.Unmarshal(out, &fileList)
-	if err != nil {
-		return err
-	}
-	readers, err := r.createFileReaders(snapshot, fileList)
-	if err != nil {
-		return err
-	}
 
 	endpoint := os.Getenv(RestoreS3EndpointEnv)
 	snapDate := snapshot.Time.Format(time.RFC3339)
@@ -194,11 +157,12 @@ func (r *RestoreStruct) s3Restore(snapshot Snapshot) error {
 	}
 
 	s3Client := s3.New(endpoint, os.Getenv(RestoreS3AccessKeyIDEnv), os.Getenv(RestoreS3SecretAccessKey))
-	err = s3Client.Connect()
+	err := s3Client.Connect()
 	if err != nil {
 		return err
 	}
-	stream := r.tarGz(readers, stats)
+
+	stream := r.compress(r.getTarReader(snapshot), stats)
 	upload := s3.UploadObject{
 		ObjectStream: stream,
 		Name:         fileName,
@@ -214,115 +178,27 @@ func (r *RestoreStruct) s3Restore(snapshot Snapshot) error {
 	return nil
 }
 
-func (r *RestoreStruct) listFilesInSnapshot(snapshot Snapshot) {
-	args := []string{"ls", "-l", "--no-lock", "--json", snapshot.ID}
-	fmt.Printf("Listing files in snapshot %v\n", snapshot.Time)
-	r.genericCommand.exec(args, commandOptions{print: false})
-}
-
-func (r *RestoreStruct) createFileReaders(snapshot Snapshot, fileList fileJSON) ([]fileNode, error) {
-	// This case is so special we can't use genericCommand for this
-	args := []string{"dump", snapshot.ID}
-
-	fmt.Println("Adding files to the restore")
-
-	filesToProcess := []fileNode{}
-
-	for _, files := range fileList {
-
-		for _, node := range files.Nodes {
-			add := false
-			var tmpFile fileNode
-			if node.Type != "dir" {
-				tmpFile = node
-				add = true
-			}
-			if add {
-				filesToProcess = append(filesToProcess, tmpFile)
-			}
-		}
-
-	}
-
-	for i := range filesToProcess {
-		tmpArgs := append(args, filesToProcess[i].Path)
-		cmd := exec.Command(restic, tmpArgs...)
-		cmd.Env = os.Environ()
-		closerChan := make(chan io.ReadCloser, 0)
-		filesToProcess[i].closer = closerChan
-		filesToProcess[i].runFunc = func() {
-
-			stdOut, err := cmd.StdoutPipe()
-			if err != nil {
-				return
-			}
-			var stdErr bytes.Buffer
-			cmd.Stderr = &stdErr
-
-			err = cmd.Start()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			closerChan <- stdOut
-			<-closerChan
-
-			err = cmd.Wait()
-			if err != nil {
-				fmt.Printf("Command failed with: '%v'\n", err)
-				fmt.Printf("Output: %v\n", stdErr.String())
-				return
-			}
-		}
-	}
-
-	return filesToProcess, nil
-}
-
-func (r *RestoreStruct) tarGz(files []fileNode, stats *restoreStats) io.Reader {
+func (r *RestoreStruct) compress(file tarStream, stats *restoreStats) io.Reader {
 	readPipe, writePipe := io.Pipe()
 	gzpWriter := gzip.NewWriter(writePipe)
-	trWriter := tar.NewWriter(gzpWriter)
 
 	go func() {
 		defer func() {
-			trWriter.Close()
 			gzpWriter.Close()
 			writePipe.Close()
 		}()
-		for _, file := range files {
-			stats.RestoredFiles = append(stats.RestoredFiles, file.Path)
-			fmt.Printf("Compressing %v...", file.Path)
-			header := &tar.Header{
-				Name:       file.Path,
-				Mode:       file.Mode,
-				Size:       file.Size,
-				Uid:        file.UID,
-				Gid:        file.Gid,
-				AccessTime: file.Atime,
-				ChangeTime: file.Ctime,
-				ModTime:    file.Mtime,
-			}
+		stats.RestoredFiles = append(stats.RestoredFiles, file.path)
 
-			err := trWriter.WriteHeader(header)
-			if err != nil {
-				fmt.Printf("\n%v\n", err)
-				r.errorMessage = err
-				return
-			}
-			go file.runFunc()
-			reader := <-file.closer
-			buffer := bufio.NewReader(reader)
-			_, err = io.Copy(trWriter, buffer)
-			if err != nil {
-				fmt.Printf("\n%v\n", err)
-				r.errorMessage = err
-				return
-			}
-			file.closer <- reader
-			fmt.Println(" done!")
+		go file.runFunc()
+		reader := <-file.readerChan
+		_, err := io.Copy(gzpWriter, reader)
+		if err != nil {
+			fmt.Printf("\n%v\n", err)
+			r.errorMessage = err
+			return
 		}
+		file.readerChan <- reader
+		fmt.Println("done!")
 	}()
 
 	return readPipe
@@ -337,4 +213,45 @@ func (r *RestoreStruct) GetWebhookData() []output.JsonMarshaller {
 
 func (r *RestoreStruct) parsePath(paths []string) string {
 	return path.Base(paths[len(paths)-1])
+}
+
+func (r *RestoreStruct) getTarReader(snapshot Snapshot) tarStream {
+	args := []string{"dump", snapshot.ID}
+
+	// Currently baas and wrestic have one path per snapshot
+	tmpArgs := append(args, snapshot.Paths[0])
+	cmd := exec.Command(restic, tmpArgs...)
+	cmd.Env = os.Environ()
+
+	readerChan := make(chan io.ReadCloser, 0)
+
+	return tarStream{
+		path:       snapshot.Paths[0],
+		readerChan: readerChan,
+		runFunc: func() {
+
+			stdOut, err := cmd.StdoutPipe()
+			if err != nil {
+				return
+			}
+			var stdErr bytes.Buffer
+			cmd.Stderr = &stdErr
+
+			err = cmd.Start()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			readerChan <- stdOut
+			<-readerChan
+
+			err = cmd.Wait()
+			if err != nil {
+				fmt.Printf("Command failed with: '%v'\n", err)
+				fmt.Printf("Output: %v\n", stdErr.String())
+				return
+			}
+		},
+	}
 }
