ff776710a9dca22f0e743a937d72404e564f19de
Chris <github.account@chrigel.net>
2021-03-30 10:43:50 +0200

    Remove preserveUnknownFields from v1 CRD
    It's being removed by the API server anyway

--- generate.go
+++ generate.go
@@ -19,7 +19,7 @@ import (
 	"os"
 )
 
-var patchFiles = []string{"v1beta1/base/backup.appuio.ch_prebackuppods.yaml", "v1/base/backup.appuio.ch_prebackuppods.yaml"}
+var patchFiles = []string{"v1beta1/base/backup.appuio.ch_prebackuppods.yaml"}
 
 // controller-gen 0.3 creates CRDs with apiextensions.k8s.io/v1beta1, but some generated properties aren't valid for that version
 // in K8s 1.18+. We would have to switch to apiextensions.k8s.io/v1, but that would make the CRD incompatible with OpenShift 3.11.
