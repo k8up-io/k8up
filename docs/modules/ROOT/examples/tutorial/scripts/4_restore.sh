223533ed87b4080b0f0545b44a3235d1ef26a165
Ramón Cahenzli <ramon.cahenzli@vshn.ch>
2021-07-13 13:41:47 +0200

    Fix quoting

--- docs/modules/ROOT/examples/tutorial/scripts/4_restore.sh
+++ docs/modules/ROOT/examples/tutorial/scripts/4_restore.sh
@@ -19,4 +19,4 @@ SNAPSHOT_ID=$(restic snapshots --json --last --path /default-mariadb | jq -r '.[
 
 # Restore MariaDB data
 MARIADB_POD=$(kubectl get pods -o custom-columns="NAME:.metadata.name" --no-headers -l "app=wordpress,tier=mariadb")
-restic dump "${SNAPSHOT_ID}" /default-mariadb | kubectl exec -i "$MARIADB_POD" -- /bin/bash -c 'mysql -uroot --password="${MARIADB_ROOT_PASSWORD}"'
+restic dump "${SNAPSHOT_ID}" /default-mariadb | kubectl exec -i "$MARIADB_POD" -- /bin/bash -c "mysql -uroot --password=\"${MARIADB_ROOT_PASSWORD}\""
