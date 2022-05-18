6887595399fe0b45218be88949bc9782f1b820bf
Daniel Hauswirth <info@chloesoe.ch>
2022-05-18 15:30:36 +0200

    Change repo URL for Minio in Tutorial
    With the new minio version having a port for API and one for GUI the
    command did not work to get the minio service URL. This commit fixes
    that and now we can export the RESTIC_REPOSITORY with a one-liner again.
    
    Signed-off-by: Daniel Hauswirth <info@chloesoe.ch>

--- docs/modules/ROOT/examples/tutorial/scripts/environment.sh
+++ docs/modules/ROOT/examples/tutorial/scripts/environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export KUBECONFIG=""
-export RESTIC_REPOSITORY=s3:$(minikube service minio --url)/backups/
+export RESTIC_REPOSITORY=s3:$(minikube service list | grep 9001 | cut -d"|" -f 5 | tr -d "[:space:]")/backups/
 export RESTIC_PASSWORD=p@ssw0rd
 export AWS_ACCESS_KEY_ID=minio
 export AWS_SECRET_ACCESS_KEY=minio123
