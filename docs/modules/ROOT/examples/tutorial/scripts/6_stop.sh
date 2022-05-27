e12a60b13853dc56abc4e35635709d6e3a5c624d
Daniel Hauswirth <info@chloesoe.ch>
2022-05-27 13:59:29 +0200

    Ensure kill port forward and update script
    environment script should have the same changes. Fix some minor stuff
    too.
    
    Signed-off-by: Daniel Hauswirth <info@chloesoe.ch>

--- docs/modules/ROOT/examples/tutorial/scripts/6_stop.sh
+++ docs/modules/ROOT/examples/tutorial/scripts/6_stop.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+kill $MINIO_PORT
+echo $MINIO_PORT
+minikube stop
 minikube delete
--- docs/modules/ROOT/examples/tutorial/scripts/environment.sh
+++ docs/modules/ROOT/examples/tutorial/scripts/environment.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+kubectl port-forward svc/minio 9000:9000 &
+export MINIO_PORT=$!
 export KUBECONFIG=""
-export RESTIC_REPOSITORY=s3:$(minikube service list | grep 9001 | cut -d"|" -f 5 | tr -d "[:space:]")/backups/
+export RESTIC_REPOSITORY=s3:http://localhost:9000/backups/
 export RESTIC_PASSWORD=p@ssw0rd
 export AWS_ACCESS_KEY_ID=minio
 export AWS_SECRET_ACCESS_KEY=minio123
