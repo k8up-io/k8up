4254e87ececf4d88ac677ab3bb4dce2a525ec600
ccremer <github.account@chrigel.net>
2022-08-15 10:35:44 +0200

    Regression: Add missing RBAC rule for events
    Apparently, with the chart move to this repo, the RBAC rules
    for the controller to create events got forgotten.
    
    Signed-off-by: ccremer <github.account@chrigel.net>

--- controllers/controller_setup.go
+++ controllers/controller_setup.go
@@ -5,6 +5,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
+// +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;create;update
+// +kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+
 // ReconcilerSetup is a common interface to configure reconcilers.
 type ReconcilerSetup interface {
 	SetupWithManager(mgr ctrl.Manager, l logr.Logger) error
