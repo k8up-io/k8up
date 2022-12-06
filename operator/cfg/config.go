dc76db283fc012a075f3fa84c588110956d807d6
ccremer <github.account@chrigel.net>
2022-12-06 14:08:00 +0100

    Revert "Deprecate operator-namespace CLI flag"
    This partially reverts commit 9698705ebe1c7138d7f8d88c0253e5b6ecf42317.
    
    Signed-off-by: ccremer <github.account@chrigel.net>

--- operator/cfg/config.go
+++ operator/cfg/config.go
@@ -78,6 +78,7 @@ type Configuration struct {
 
 	// Enabling this will ensure there is only one active controller manager.
 	EnableLeaderElection bool
+	OperatorNamespace    string
 
 	// Allows to pass options to restic, see https://restic.readthedocs.io/en/stable/manual_rest.html?highlight=--option#usage-help
 	// Format: `key=value,key2=value2`
