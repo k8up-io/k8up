88c73578df4cd1911edf4f01eef0db7e7f73dbff
ccremer <github.account@chrigel.net>
2022-12-09 11:21:00 +0100

    Make sure the restic version stays on-par with go.mod
    Signed-off-by: ccremer <github.account@chrigel.net>

--- Makefile.restic-integration.vars.mk
+++ Makefile.restic-integration.vars.mk
@@ -13,7 +13,7 @@ restore_dir ?= $(integrationtest_dir)/restore
 
 stats_url ?= http://localhost:8091
 
-restic_version ?= 0.12.1
+restic_version ?= $(shell go mod edit -json | jq -r '.Require[] | select(.Path == "github.com/restic/restic").Version' | sed "s/v//")
 restic_path ?= $(go_bin)/restic
 restic_pid ?= $(integrationtest_dir)/restic.pid
 restic_url ?= https://github.com/restic/restic/releases/download/v$(restic_version)/restic_$(restic_version)_$(os)_$(arch).bz2
