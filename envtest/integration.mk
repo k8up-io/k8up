21609cc940105307c8dd65388288faae4418c12b
Manuel Hutter <manuel@hutter.io>
2023-10-27 10:08:30 +0200

    chore: remove commented-out code
    ¯\_(ツ)_/¯
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- envtest/integration.mk
+++ envtest/integration.mk
@@ -32,13 +32,6 @@ integration-test: $(setup_envtest_bin) generate restic-integration-test-setup .e
 	export KUBEBUILDER_ASSETS="$$($(setup_envtest_bin) $(ENVTEST_ADDITIONAL_FLAGS) use -i -p path '$(ENVTEST_K8S_VERSION)!')" && \
 	go test -tags=integration -coverprofile cover.out -covermode atomic ./...
 
-# .PHONY: integration-test
-# integration-test: $(setup_envtest_bin) .envtest_crds ## Run integration tests against code
-# 	$(setup_envtest_bin) $(ENVTEST_ADDITIONAL_FLAGS) use '$(ENVTEST_K8S_VERSION)!'
-# 	@chmod -R +w $(go_bin)/k8s
-# 	export KUBEBUILDER_ASSETS="$$($(setup_envtest_bin) $(ENVTEST_ADDITIONAL_FLAGS) use -i -p path '$(ENVTEST_K8S_VERSION)!')" && \
-# 	go test -tags=integration -coverprofile cover.out -covermode atomic ./...
-
 $(envtest_crd_dir):
 	@mkdir -p $@
 
