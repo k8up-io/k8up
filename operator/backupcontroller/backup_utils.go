1fc3b16762c5f9e9c855ff2b1c64d61bfd869f71
poyaz <pooya_azarpour@yahoo.com>
2024-04-11 17:32:05 +0330

    [UPDATE] Rename argument "--varDir" to "-varDir"
    Signed-off-by: poyaz <pooya_azarpour@yahoo.com>

--- operator/backupcontroller/backup_utils.go
+++ operator/backupcontroller/backup_utils.go
@@ -75,7 +75,7 @@ func (b *BackupExecutor) createServiceAccountAndBinding(ctx context.Context) err
 }
 
 func (b *BackupExecutor) setupArgs() []string {
-	args := []string{"--varDir", cfg.Config.PodVarDir}
+	args := []string{"-varDir", cfg.Config.PodVarDir}
 	if len(b.backup.Spec.Tags) > 0 {
 		args = append(args, executor.BuildTagArgs(b.backup.Spec.Tags)...)
 	}
