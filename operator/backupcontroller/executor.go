34c91041cedafb0c3ee5d77ce49ef1caf6d0c8ef
Johannes Bornhold <johannes@bornhold.name>
2024-06-07 01:31:16 +0200

    Ignore terminated Pods in mapping of PVCs to Pods
    Signed-off-by: Johannes Bornhold <johannes@bornhold.name>

--- operator/backupcontroller/executor.go
+++ operator/backupcontroller/executor.go
@@ -69,6 +69,10 @@ func (b *BackupExecutor) listAndFilterPVCs(ctx context.Context, annotation strin
 		return nil, fmt.Errorf("list pods: %w", err)
 	}
 	for _, pod := range pods.Items {
+		if pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed {
+			log.V(1).Info("Ignoring terminated Pod", "pod", pod.GetName())
+			continue
+		}
 		for _, volume := range pod.Spec.Volumes {
 			if volume.PersistentVolumeClaim != nil {
 				pvcPodMap[volume.PersistentVolumeClaim.ClaimName] = pod
